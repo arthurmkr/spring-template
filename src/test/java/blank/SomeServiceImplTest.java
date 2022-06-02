f4a5b1a92cb06da6e46227c9ec46d4c340a2421a
Artur Mkrtchyan <arthurmk@gmail.com>
2022-06-02 10:31:42 +0300

    attempt to override production beans

--- src/test/java/blank/SomeServiceImplTest.java
+++ src/test/java/blank/SomeServiceImplTest.java
@@ -2,13 +2,17 @@ package blank;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.TestPropertySource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Import(TestConfig.class)
 @SpringBootTest
+@TestPropertySource(locations= "classpath:application-test.properties")
 class SomeServiceImplTest {
     @Autowired
     SomeService service;
--- src/test/java/blank/TestConfig.java
+++ src/test/java/blank/TestConfig.java
@@ -2,6 +2,8 @@ package blank;
 
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @TestConfiguration
 class TestConfig {
